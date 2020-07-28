7f209e1c0e2daf7cc074ed5f84fcf6564aa4b2f1
kdmp <nick@nbolton.com>
2020-07-28 11:14:08 +0100

    M3: Spawn every 2 seconds for 10 seconds (6 spawns)

--- Horus/Missions/Mission 003/Mission.lua
+++ Horus/Missions/Mission 003/Mission.lua
@@ -5,14 +5,13 @@ BASE:TraceLevel(1)
 hlog("Script start")
 
 transport = SPAWN:New("Transport")
-transport:SpawnScheduled(2, 0)
 
 function helloScheduler()
   hlog("Scheduled function start")
-  transport:SpawnScheduleStop()
+  transport:Spawn()
   hlog("Scheduled function end")
 end
 
-SCHEDULER:New(nil, helloScheduler, {}, 2, 0, 0, 0)
+SCHEDULER:New(nil, helloScheduler, {}, 0, 2, 0, 10)
 
 hlog("Script end")
