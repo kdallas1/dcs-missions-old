bb36f4e3a2aad02f8a1338105ed0ba21daf0c518
kdmp <nick@nbolton.com>
2020-07-28 11:22:11 +0100

    M3: Use existing moose feature to spawn 3 transports (simplify code)

--- Horus/Missions/Mission 003/Mission.lua
+++ Horus/Missions/Mission 003/Mission.lua
@@ -4,22 +4,6 @@ BASE:TraceLevel(1)
 
 hlog("Script start")
 
-transport = SPAWN:New("Transport")
-transportCount = 0
-maxTransports = 3
-
-function spawnTransport()
-  hlog("Spawn transport start")
-  transport:Spawn()
-  transportCount = incr(transportCount)
-  
-  if (transportCount < maxTransports) then
-    SCHEDULER:New(nil, spawnTransport, {}, 2, 0, 0, 0)
-  end
-  
-  hlog("Spawn transport end")
-end
-
-SCHEDULER:New(nil, spawnTransport, {}, 0, 0, 0, 0)
+transport = SPAWN:New("Transport"):InitLimit(3,3):SpawnScheduled(2,0)
 
 hlog("Script end")
