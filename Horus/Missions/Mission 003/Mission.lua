ae736aac0d3e2a51477ca1278871a40ed2a9d2fb
kdmp <nick@nbolton.com>
2020-07-28 11:18:42 +0100

    M3: Spawn 3 transports

--- Horus/Missions/Mission 003/Mission.lua
+++ Horus/Missions/Mission 003/Mission.lua
@@ -5,13 +5,21 @@ BASE:TraceLevel(1)
 hlog("Script start")
 
 transport = SPAWN:New("Transport")
+transportCount = 0
+maxTransports = 3
 
-function helloScheduler()
-  hlog("Scheduled function start")
+function spawnTransport()
+  hlog("Spawn transport start")
   transport:Spawn()
-  hlog("Scheduled function end")
+  transportCount = incr(transportCount)
+  
+  if (transportCount < maxTransports) then
+    SCHEDULER:New(nil, spawnTransport, {}, 2, 0, 0, 0)
+  end
+  
+  hlog("Spawn transport end")
 end
 
-SCHEDULER:New(nil, helloScheduler, {}, 0, 2, 0, 10)
+SCHEDULER:New(nil, spawnTransport, {}, 0, 0, 0, 0)
 
 hlog("Script end")
--- Horus/Source/Global.lua
+++ Horus/Source/Global.lua
@@ -3,3 +3,7 @@ tag = "Horus: "
 function hlog(line)
   env.info(tag .. line)
 end
+
+function incr(i)
+  return i + 1
+end
