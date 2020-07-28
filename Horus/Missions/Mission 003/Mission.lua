46c63ecfc3cb2398af997836cf0e7e31d115ca04
kdmp <nick@nbolton.com>
2020-07-28 20:21:26 +0100

    M3: Win/lose conditions done

--- Horus/Missions/Mission 003/Mission.lua
+++ Horus/Missions/Mission 003/Mission.lua
@@ -14,6 +14,18 @@ transportSpawn = SPAWN:New("Transport"):InitLimit(100, transportCount):SpawnSche
 winCheckInterval = 10
 landTestPlayersDone = false
 playerOnline = false
+winLoseDone = false
+
+function killTransport()
+  group = transportSpawn:GetGroupFromIndex(1)
+  if group then
+    unit = group:GetUnit(1)
+    unit:Explode(100, 0)
+  end
+end
+
+local menu = MENU_COALITION:New(coalition.side.BLUE, "Debug")
+MENU_COALITION_COMMAND:New(coalition.side.BLUE, "Kill transport", menu, killTransport)
 
 function landTestPlayers(playerGroup)
   hTrace(2, "landing test players")
@@ -21,6 +33,40 @@ function landTestPlayers(playerGroup)
   local land = airbase:GetCoordinate():WaypointAirLanding(300, airbase)
   local route = { land }
   playerGroup:Route(route)
+end
+
+function announceWin()
+  MESSAGE:New("Mission accomplished!", 100):ToAll()
+  USERSOUND:New("MissionAccomplished.ogg"):ToAll()
+  winLoseDone = true
+end
+
+function announceLose()
+  MESSAGE:New("Mission failed!", 100):ToAll()
+  USERSOUND:New("MissionFailed.ogg"):ToAll()
+  winLoseDone = true
+end
+
+function onTransportCrashed(e)
+  MESSAGE:New("Transport crashed!", 100):ToAll()
+  if (not winLoseDone) then
+    announceLose()
+  end
+end
+
+function onTransportHit(e)
+  MESSAGE:New("Transport hit!", 100):ToAll()
+end
+
+events = EVENTHANDLER:New()
+events:HandleEvent(EVENTS.Birth)
+
+function events:OnEventBirth(e)
+  unit = e.IniUnit
+  hTrace(1, "Birth: " .. unit:GetName())
+  
+  unit:HandleEvent(EVENTS.Crash, onTransportCrashed)
+  unit:HandleEvent(EVENTS.Hit, onTransportHit)
 end
 
 Messager = SCHEDULER:New(nil,
@@ -38,11 +84,15 @@ Messager = SCHEDULER:New(nil,
   
     local playersAreParked = groupIsParked(nalchikParkZone, playerGroup)
     local transportsAreParked =  spawnGroupsAreParked(nalchikParkZone, transportSpawn, transportCount)
-    local parked = playersAreParked and transportsAreParked 
+    local everyoneParked = playersAreParked and transportsAreParked
     
     hTrace(1, (playersAreParked and "✔ Players: All parked" or "❌ Players: Not all parked"), 1)
     hTrace(1, (transportsAreParked and "✔ Transports: All parked" or "❌ Transports: Not all parked"), 1)
-    hTrace(1, (parked and "✔ Everyone: All parked" or "❌ Everyone: Not all parked"), 1)
+    hTrace(1, (everyoneParked and "✔ Everyone: All parked" or "❌ Everyone: Not all parked"), 1)
+    
+    if (everyoneParked and not winLoseDone) then
+      announceWin()
+    end
     
     if (transportsAreParked and not landTestPlayersDone) then
       landTestPlayers(playerGroup)
