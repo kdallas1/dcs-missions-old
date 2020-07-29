01384d22b38f9c78fd205305bdf1f78104ecf1a8
kdmp <nick@nbolton.com>
2020-07-29 22:18:28 +0100

    M3: Increased MiG separation

--- Horus/Source/Mission003.lua
+++ Horus/Source/Mission003.lua
@@ -13,7 +13,7 @@ local _maxUnitsAlive = 100
 local _transportSeparation = 200
 local _transportVariation = .5
 local _migsPerAirbase = 4
-local _migsSpawnSeparation = 300
+local _migsSpawnSeparation = 400
 local _migsSpawnVariation = .5
 local _transportMinLife = 30
 
