a73cce2764fc598fb05b5f714868d963c6206d30
kdmp <nick@nbolton.com>
2020-08-07 09:33:44 +0100

    Extra tests for inheritance

--- Horus/Source/KD/Test/TestObject.lua
+++ Horus/Source/KD/Test/TestObject.lua
@@ -1,3 +1,5 @@
+dofile(baseDir .. "KD/Object.lua")
+
 local function Test_ObjectsAreUnique()
 
   Child1 = { }
@@ -13,8 +15,35 @@ local function Test_ObjectsAreUnique()
   
 end
 
+local function Test_ObjectsInherit()
+
+  Child1 = { foo = "test" }
+  Child1 = createClass(Child1, Object)
+  local c = Child1:New()
+  
+  TestAssert(c.foo, "Expected field from Child1")
+  TestAssert(c.foo == "test", "Expected string from Child1")
+  
+end
+
+local function Test_ObjectsInheritDeeply()
+
+  Child1 = { foo = "test" }
+  Child2 = { bar = "test" }
+  Child3 = { baz = "test" }
+  Child3 = createClass(Child3, Child2, Child1, Object)
+  local c = Child3:New()
+  
+  TestAssert(c.foo == "test", "Expected string from Child1")
+  TestAssert(c.bar == "test", "Expected string from Child2")
+  TestAssert(c.baz == "test", "Expected string from Child3")
+  
+end
+
 function Test_Object()
   return RunTests {
-    Test_ObjectsAreUnique
+    Test_ObjectsInherit,
+    Test_ObjectsInheritDeeply,
+    Test_ObjectsAreUnique,
   }
 end
