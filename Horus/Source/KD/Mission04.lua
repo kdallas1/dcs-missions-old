738431387dea2bb04bc2f5dad7799335a351826d
kdmp <nick@nbolton.com>
2020-08-10 18:41:00 +0100

    M4: Activate enemy helos and fail on friendly helo dead

--- Horus/Source/KD/Mission04.lua
+++ Horus/Source/KD/Mission04.lua
@@ -9,22 +9,14 @@ dofile(baseDir .. "KD/Mission.lua")
 Mission04 = {
   className = "Mission04",
 
-  traceLevel = 1,
+  traceLevel = 3,
   
-  --- @field #Wrapper.Group#GROUP playerGroup
   playerGroup = nil,
-  
-  --- @field #Wrapper.Group#GROUP friendlyHeloGroup
   friendlyHeloGroup = nil,
-  
-  --- @field #Wrapper.Group#GROUP enemyHeloGroup
   enemyHeloGroup = nil,
-  
-  --- @field #Wrapper.Zone#ZONE extractionLandZone 
-  extractionLandZone = nil
+  extractionLandZone = nil,
+  extractionZone = nil
 }
-
-Mission04 = createClass(Mission04, Mission)
 
 ---
 -- @param #Mission04 self
@@ -34,24 +26,22 @@ function Mission04:Mission04()
   self.friendlyHeloGroup = GROUP:FindByName("Friendly Helos")
   self.enemyHeloGroup = GROUP:FindByName("Enemy Helos")
   self.extractionLandZone = ZONE:New("Extraction Land")
+  self.extractionZone = ZONE:New("Extraction")
   
   self:Assert(self.playerGroup, "Player group not found")
   self:Assert(self.friendlyHeloGroup, "Friendly helo group not found")
   self:Assert(self.enemyHeloGroup, "Enemy helo group not found")
+  
+  self:AddGroup(self.friendlyHeloGroup)
+  self:AddGroup(self.enemyHeloGroup)
 end
 
 ---
 -- @param #Mission04 self
 function Mission04:OnStart()
   
-  -- TODO: delay as this only lasts for a short period
-  --self.extractionLandZone:SmokeZone(SMOKECOLOR.Red)
-  
   -- TODO: trigger activate when players come close
   self.friendlyHeloGroup:Activate(0)
-  
-  -- TODO: trigger when friendly helo lands
-  --self.enemyHeloGroup:Activate(0)
   
   MESSAGE:New("Mission 4: Assist rescue of downed recon pilots", self.messageTimeShort):ToAll()
   MESSAGE:New("Read the mission brief before takeoff", self.messageTimeShort):ToAll()
@@ -60,6 +50,44 @@ end
 
 ---
 -- @param #Mission04 self
+-- @param Wrapper.Unit#UNIT unit
+function Mission04:OnUnitDead(unit)
+  self:Trace(1, "Unit dead: " .. unit:GetName())
+    
+  if (string.match(unit:GetName(), "Friendly Helo")) then
+    
+    self:Trace(1, "Friendly helo destroyed: " .. unit:GetName())
+    self:MessageAll(MessageLength.Long, "Friendly helo destroyed!")
+    self:PlaySound(Sound.UnitLost)
+    
+    if (not self.winLoseDone) then
+      self:AnnounceLose(2)
+    end
+    
+  end
+end
+
+---
+-- @param #Mission04 self
 function Mission04:OnGameLoop()
   
+  if ((not self.enemyHelosActivated) and self.friendlyHeloGroup:IsCompletelyInZone(self.extractionZone)) then
+    self.enemyHeloGroup:Activate(0)
+    self:PlaySound(Sound.EnemyApproching, 0)
+    self:MessageAll(MessageLength.Short, "Enemy attack helos inbound from Nalchik to Fahrn, ETA 2 minutes")
+    self:Trace(1, "Enemy helos activated")
+    self.enemyHelosActivated = true
+  end
+  
+  if ((not self.friendlyHelosSignalled) and self:GroupIsParked(self.extractionLandZone, self.friendlyHeloGroup)) then 
+      self.friendlyHeloGroup:SmokeRed()
+      self:MessageAll(MessageLength.Short, "Friendly helos landed Fahrn, extraction T-1 minute")
+      self:Trace(1, "Friendly helos landed")
+      self.friendlyHelosSignalled = true
+  end
+  
+  self:SelfDestructDamagedUnitsInList(self.friendlyHeloGroup:GetUnits(), 10)
+  
 end
+
+Mission04 = createClass(Mission04, Mission)
