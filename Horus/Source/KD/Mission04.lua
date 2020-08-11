7a6c11f4b83bfc5f7e5a74ba61bcc7ecf6e39181
kdmp <nick@nbolton.com>
2020-08-11 18:47:23 +0100

    M4: State machine for helo rendezvous and extraction

--- Horus/Source/KD/Mission04.lua
+++ Horus/Source/KD/Mission04.lua
@@ -9,42 +9,107 @@ dofile(baseDir .. "KD/Mission.lua")
 Mission04 = {
   className = "Mission04",
 
-  traceLevel = 3,
-  
-  playerGroup = nil,
-  friendlyHeloGroup = nil,
-  enemyHeloGroup = nil,
-  extractionLandZone = nil,
-  extractionZone = nil
+  traceLevel = 2
+}
+
+---
+-- @type Mission04.State
+-- @extends KD.StateMachine#State
+Mission04.State = {
+  MissionAccomplished       = 0,
+  MissionFailed             = 1,
+  HeloRendezvousDone        = 2,
+  FriendlyHelosSignalled    = 3,
+  EnemyHelosActivated       = 4,
+  ExtractionComplete        = 5
+}
+
+---
+-- @type Mission04.Flags
+Mission04.Flags = {
+  FriendlyHelosAdvance      = 10,
 }
 
 ---
 -- @param #Mission04 self
 function Mission04:Mission04()
-  
+
   self.playerGroup = GROUP:FindByName("Dodge Squadron")
   self.friendlyHeloGroup = GROUP:FindByName("Friendly Helos")
   self.enemyHeloGroup = GROUP:FindByName("Enemy Helos")
   self.extractionLandZone = ZONE:New("Extraction Land")
   self.extractionZone = ZONE:New("Extraction")
+  self.rendezvousZone = ZONE:New("Rendezvous")
   
   self:Assert(self.playerGroup, "Player group not found")
   self:Assert(self.friendlyHeloGroup, "Friendly helo group not found")
   self:Assert(self.enemyHeloGroup, "Enemy helo group not found")
+  self:Assert(self.extractionLandZone, "Extraction land zone not found")
+  self:Assert(self.extractionZone, "Extraction zone not found")
+  self:Assert(self.rendezvousZone, "Rendezvous zone not found")
   
   self:AddGroup(self.friendlyHeloGroup)
   self:AddGroup(self.enemyHeloGroup)
+  
+  self.state = StateMachine:New()
+  
+  self.state:ActionOnce(
+    Mission04.State.MissionAccomplished,
+    function() self:AnnounceWin(2) end
+  )
+  
+  self.state:ActionOnce(
+    Mission04.State.MissionFailed,
+    function() self:AnnounceLose(2) end
+  )
+  
+  self.state:TriggerOnce(
+    Mission04.State.HeloRendezvousDone,
+    function() return self.playerGroup:IsAnyInZone(self.rendezvousZone) end,
+    function() self:OnHeloRendezvousDone() end
+  )
+  
+  self.state:TriggerOnce(
+    Mission04.State.EnemyHelosActivated,
+    function() return self.friendlyHeloGroup:IsCompletelyInZone(self.extractionZone) end,
+    function() self:OnEnemyHelosActivated() end
+  )
+  
+  self.state:TriggerOnce(
+    Mission04.State.FriendlyHelosSignalled,
+    function() return self:GroupIsParked(self.extractionLandZone, self.friendlyHeloGroup) end,
+    function() self:OnFriendlyHelosSignalled() end
+  )
+  
+  self.state:TriggerOnceAfter(
+    Mission04.State.ExtractionComplete,
+    Mission04.State.FriendlyHelosSignalled,
+    function() return self.friendlyHeloGroup:IsNotInZone(self.extractionZone) end,
+    function() self:OnExtractionComplete() end
+  )
+  
 end
 
 ---
 -- @param #Mission04 self
 function Mission04:OnStart()
   
-  -- TODO: trigger activate when players come close
   self.friendlyHeloGroup:Activate(0)
   
-  MESSAGE:New("Mission 4: Assist rescue of downed recon pilots", self.messageTimeShort):ToAll()
-  MESSAGE:New("Read the mission brief before takeoff", self.messageTimeShort):ToAll()
+  self:MessageAll(MessageLength.Long, "Mission 4: Assist rescue of downed recon pilots")
+  self:MessageAll(MessageLength.Long, "Read the mission brief before takeoff")
+  
+end
+
+--- 
+-- @param #Mission04 self
+function Mission04:OnHeloRendezvousDone()
+  
+  self:PlaySound(Sound.ForKingAndCountry)
+  self:MessageAll(MessageLength.Short, "Rendezvous complete, helos proceeding to extraction")
+  
+  -- ME waypoint stop condition
+  self:SetFlag(Mission04.Flags.FriendlyHelosAdvance, true)
   
 end
 
@@ -52,6 +117,7 @@ end
 -- @param #Mission04 self
 -- @param Wrapper.Unit#UNIT unit
 function Mission04:OnUnitDead(unit)
+
   self:Trace(1, "Unit dead: " .. unit:GetName())
     
   if (string.match(unit:GetName(), "Friendly Helo")) then
@@ -59,33 +125,38 @@ function Mission04:OnUnitDead(unit)
     self:Trace(1, "Friendly helo destroyed: " .. unit:GetName())
     self:MessageAll(MessageLength.Long, "Friendly helo destroyed!")
     self:PlaySound(Sound.UnitLost)
-    
-    if (not self.winLoseDone) then
-      self:AnnounceLose(2)
-    end
+    self.state:Change(Mission04.State.MissionFailed)
     
   end
+  
+end
+
+---
+-- @param #Mission04 self
+function Mission04:OnEnemyHelosActivated()
+
+  self.enemyHeloGroup:Activate(0)
+  self:PlaySound(Sound.EnemyApproching, 0)
+  self:MessageAll(MessageLength.Short, "Enemy attack helos inbound from Nalchik to Fahrn, ETA 2 minutes")
+  self:Trace(1, "Enemy helos activated")
+  
+end
+
+---
+-- @param #Mission04 self
+function Mission04:FriendlyHelosSignalled()
+
+  self.friendlyHeloGroup:SmokeRed()
+  self:MessageAll(MessageLength.Short, "Friendly helos landed Fahrn, extraction T-1 minute")
+  self:Trace(1, "Friendly helos landed")
+  
 end
 
 ---
 -- @param #Mission04 self
 function Mission04:OnGameLoop()
   
-  if ((not self.enemyHelosActivated) and self.friendlyHeloGroup:IsCompletelyInZone(self.extractionZone)) then
-    self.enemyHeloGroup:Activate(0)
-    self:PlaySound(Sound.EnemyApproching, 0)
-    self:MessageAll(MessageLength.Short, "Enemy attack helos inbound from Nalchik to Fahrn, ETA 2 minutes")
-    self:Trace(1, "Enemy helos activated")
-    self.enemyHelosActivated = true
-  end
-  
-  if ((not self.friendlyHelosSignalled) and self:GroupIsParked(self.extractionLandZone, self.friendlyHeloGroup)) then 
-      self.friendlyHeloGroup:SmokeRed()
-      self:MessageAll(MessageLength.Short, "Friendly helos landed Fahrn, extraction T-1 minute")
-      self:Trace(1, "Friendly helos landed")
-      self.friendlyHelosSignalled = true
-  end
-  
+  self.state:CheckTriggers()
   self:SelfDestructDamagedUnitsInList(self.friendlyHeloGroup:GetUnits(), 10)
   
 end
