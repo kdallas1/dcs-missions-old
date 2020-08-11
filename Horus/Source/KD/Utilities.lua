148ec3869785dc0c83c3205aec92a737ae858ea3
kdmp <nick@nbolton.com>
2020-08-11 19:14:24 +0100

    Added table/list shuffle alias (same thing)

--- Horus/Source/KD/Utilities.lua
+++ Horus/Source/KD/Utilities.lua
@@ -1,17 +1,26 @@
+Table = {}
 List = {}
 Boolean = {}
 
 --- 
 -- @param #list list
-function List:Shuffle(list)
-  Object:AssertType(list, "list")
-  
-  for i = #list, 2, -1 do
+function Table:Shuffle(table)
+  Object:AssertType(table, "table")
+  for i = #table, 2, -1 do
     local j = math.random(i)
-    list[i], list[j] = list[j], list[i]
+    table[i], table[j] = table[j], table[i]
   end
 end
 
+--- 
+-- @param #list list
+function List:Shuffle(list)
+  -- In Lua, lists are tables
+  Table:Shuffle(list)
+end
+
+--- 
+-- @param #boolean value
 function Boolean:ToString(value)
   Object:AssertType(value, "boolean")
   if value then return "True" else return "False" end 
