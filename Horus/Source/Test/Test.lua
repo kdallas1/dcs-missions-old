2db0676bd5d0a71ee02ea3f1ecbd24bcee4715cf
Korben Dallas <nick@nbolton.com>
2020-08-21 09:06:09 +0100

    M6: Started on Mission 6

--- Horus/Source/Test/Test.lua
+++ Horus/Source/Test/Test.lua
@@ -1,8 +1,10 @@
 dofile(baseDir .. "Horus/Source/Test/TestMission05.lua")
+dofile(baseDir .. "Horus/Source/Test/TestMission06.lua")
 
 function Test_Horus()
   RunTests {
     "Horus",
-    Test_Mission05
+    Test_Mission05,
+    Test_Mission06,
   }
 end
