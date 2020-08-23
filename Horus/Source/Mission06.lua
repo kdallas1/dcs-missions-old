9138ee7f8b148880517e0f3d6d586e883c621f1b
Korben Dallas <nick@nbolton.com>
2020-08-23 21:08:02 +0100

    M6: Mission accomplished condition

--- Horus/Source/Mission06.lua
+++ Horus/Source/Mission06.lua
@@ -26,6 +26,13 @@ Mission06 = {
 }
 
 ---
+-- @type Mission06.State
+-- @extends KD.Mission#MissionState
+Mission06.State = {
+  EnemyFobsDead        = State:NextState(),
+}
+
+---
 -- @param #Mission06 self
 function Mission06:Mission06()
 
@@ -35,9 +42,26 @@ function Mission06:Mission06()
   end
 
   self.friendlyBase = {}
-  for i = 1, self.enemyFobMax do
+  for i = 1, self.friendlyBaseMax do
     self.friendlyBase[i] = self:NewFriendlyBase(i)
   end
+
+  self.nalchikParkZone = self.moose.zone:New("Nalchik Park")
+  self:Assert(self.nalchikParkZone, "Nalchik park zone not found")
+
+  self.state:TriggerOnce(
+    Mission06.State.EnemyFobsDead,
+    function() return (self:EnemyFobAliveCount() == 0) end
+  )
+
+  self.state:TriggerOnceAfter(
+    MissionState.MissionAccomplished,
+    Mission06.State.EnemyFobsDead,
+    function()
+      local parked = self:UnitsAreParked(self.nalchikParkZone, self.players)
+      return parked
+     end
+  )
 
 end
 
@@ -58,6 +82,11 @@ function Mission06:NewEnemyFob(i)
   self:Assert(fob.heloSpawn, fobName .. " Helos not found")
 
   fob.command.enemyFob = fob
+
+  self:AddGroup(fob.sam)
+  self:AddUnit(fob.command)
+  self:AddSpawner(fob.tankSpawn)
+  self:AddSpawner(fob.heloSpawn)
 
   self.moose.scheduler:New(nil, function()
     fob.tankSpawn:Spawn()
@@ -85,6 +114,11 @@ function Mission06:NewFriendlyBase(i)
   self:Assert(base.heloSpawn, baseName .. " Helos not found")
 
   base.command.friendlyBase = base
+
+  self:AddGroup(base.sam)
+  self:AddUnit(base.command)
+  self:AddSpawner(base.tankSpawn)
+  self:AddSpawner(base.heloSpawn)
 
   self.moose.scheduler:New(nil, function()
     base.tankSpawn:Spawn()
@@ -119,14 +153,14 @@ function Mission06:OnUnitDamaged(unit)
   if (unit.friendlyBase) then
 
     self:PlaySound(Sound.OurBaseIsUnderAttack)
-    self:MessageAll(MessageLength.Long, unit.friendlyBase.name + " is under attack! Health: " .. unit.GetLife())
+    self:MessageAll(MessageLength.Long, unit.friendlyBase.name .. " is under attack! Health: " .. unit.GetLife())
 
   end
 
   if (unit.enemyFob) then
 
     self:PlaySound(Sound.ShakeItBaby)
-    self:MessageAll(MessageLength.Short, unit.enemyFob.name + " is taking damage. Health: " .. unit.GetLife())
+    self:MessageAll(MessageLength.Short, unit.enemyFob.name .. " is taking damage. Health: " .. unit.GetLife())
 
   end
 
@@ -142,18 +176,30 @@ function Mission06:OnUnitDead(unit)
   if (unit.friendlyBase) then
 
     self:PlaySound(Sound.StructureDestoyed)
-    self:MessageAll(MessageLength.Long, unit.friendlyBase.name + " has been captured!")
-    self.state.Change(MissionState.MissionFailed)
+    self:MessageAll(MessageLength.Long, unit.friendlyBase.name .. " has been captured!")
+    self.state:Change(MissionState.MissionFailed)
 
   end
 
   if (unit.enemyFob) then
 
     self:PlaySound(Sound.KissItByeBye)
-    self:MessageAll(MessageLength.Short, unit.enemyFob.name + " has been destroyed!")
+    self:MessageAll(MessageLength.Short, unit.enemyFob.name .. " has been destroyed!")
     unit.enemyFob.dead = true
 
   end
+
+end
+
+function Mission06:EnemyFobAliveCount()
+
+  local count = 0
+  for i = 1, #self.enemyFob do
+    if not self.enemyFob[i].dead then
+      count = count + 1
+    end
+  end
+  return count
 
 end
 
