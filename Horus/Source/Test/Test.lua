745394be595a50c7604855fd5b58f0353ec987e5
Korben Dallas <nick@nbolton.com>
2020-08-24 20:57:08 +0100

    M9: Started mission

--- Horus/Source/Test/Test.lua
+++ Horus/Source/Test/Test.lua
@@ -1,10 +1,16 @@
 dofile(baseDir .. "Horus/Source/Test/TestMission05.lua")
 dofile(baseDir .. "Horus/Source/Test/TestMission06.lua")
+dofile(baseDir .. "Horus/Source/Test/TestMission07.lua")
+dofile(baseDir .. "Horus/Source/Test/TestMission08.lua")
+dofile(baseDir .. "Horus/Source/Test/TestMission09.lua")
 
 function Test_Horus()
   RunTests {
     "Horus",
     Test_Mission05,
     Test_Mission06,
+    Test_Mission07,
+    Test_Mission08,
+    Test_Mission09,
   }
 end
