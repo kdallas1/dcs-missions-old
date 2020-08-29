49ccbe38a2fc674da5e5b7f3224eff0458bc0c5b
Korben Dallas <nick@nbolton.com>
2020-08-29 14:27:30 +0100

    M7: Changed logic to use 1 spawner

--- Source/Horus/Mission07.lua
+++ Source/Horus/Mission07.lua
@@ -9,31 +9,41 @@ dofile(baseDir .. "KD/Mission.lua")
 Mission07 = {
   className = "Mission07",
 
-  enemyJetSpawnCount = 10,
   enemyJetInfoStart = 10,
-  enemyJetSpawnStart = 300
+  enemyJetSpawnStart = 300,
+  enemyJetSpawnSeparation = 300,
+  enemyJetSpawnMaxPerPlayer = 3,
+  enemyJetSpawnDelay = 60
 }
 
 ---
 -- @type Mission07.State
 -- @extends KD.Mission#MissionState
 Mission07.State = {
+  EnemyJetsDestroyed = State:NextState(),
 }
 
 ---
 -- @param #Mission07 self
 function Mission07:Mission07()
 
-  self.nalchikParkZone = self:NewMooseZone("Nalchik Park")
-  
-  self.enemyJetSpawns = {}
-  for i = 1, self.enemyJetSpawnCount do
-    self.enemyJetSpawns[i] = self:NewMooseSpawn("Enemy Jet " .. self:GetDcsNumber(i))
-  end
+  self.mozdokParkZone = self:NewMooseZone("Mozdok Park")
+  self.enemyJets = self:NewMooseSpawn("Enemy Jets")
 
   self.state:TriggerOnce(
+    Mission07.State.EnemyJetsDestroyed,
+    function() return (self:CountAliveUnitsFromSpawn(self.enemyJets) == 0) end,
+    function()
+      self:MessageAll(MessageLength.Long, "First objective met. Land at Mozdok.")
+      self:PlaySound(Sound.FirstObjectiveMet)
+      self.enemyJets:SpawnScheduleStop()
+    end
+  )
+
+  self.state:TriggerOnceAfter(
     MissionState.MissionAccomplished,
-    function() return self:CountEnemyJets() == 0 end
+    Mission07.State.EnemyJetsDestroyed,
+    function() return self:UnitsAreParked(self.mozdokParkZone, self.players) end
   )
 
 end
@@ -47,23 +57,23 @@ function Mission07:OnStart()
 
   self.moose.scheduler:New(nil, function()
 
+    -- TODO: is a cold start actually 5 mins?
+    self:MessageAll(MessageLength.Short, "Enemy jets are being mobilised in T-5 minutes.")
     self:PlaySound(Sound.MissionTimerInitialised)
-    self:MessageAll(MessageLength.Short, "Enemy fighters are being mobilised in T-5 minutes.")
+
+    local enemyJetsMax = #self.players * self.enemyJetSpawnMaxPerPlayer
+    local startCount = enemyJetsMax - self:CountAliveUnitsFromSpawn(self.enemyJets)
+    self:SpawnEnemyJets(startCount)
+
+    self.moose.scheduler:New(nil, function()
+  
+      self:MessageAll(MessageLength.Short, "Enemy jets have been mobilised, takeoff in 60 seconds.")
+      self:PlaySound(Sound.EnemyApproching)
+  
+    end, {}, self.enemyJetSpawnStart, self.enemyJetSpawnSeparation)
 
   end, {}, self.enemyJetInfoStart)
 
-  self.moose.scheduler:New(nil, function()
-
-    self:PlaySound(Sound.EnemyApproching)
-    self:MessageAll(MessageLength.Short, "Enemy fighters have been mobilised, takeoff in 60 seconds.")
-
-  end, {}, self.enemyJetInfoStart + self.enemyJetSpawnStart)
-
-end
-
----
--- @param #Mission07 self
-function Mission07:OnGameLoop()
 end
 
 ---
@@ -79,18 +89,28 @@ function Mission07:OnUnitDead(unit)
     
       self:MessageAll(MessageLength.Short, "Enemy jet destroyed. Remaining: " .. self:CountEnemyJets())
       self:PlayEnemyDeadSound()
+
+      self.moose.scheduler:New(nil, function()
+        self:SpawnEnemyJets(1)
+      end, {}, self.enemyJetSpawnDelay)
   
     end
     
   end
 end
 
-function Mission07:CountEnemyJets()
-  local count = 0
-  for i = 1, #self.enemyJetSpawns do
-    count = count + self:CountAliveUnitsFromSpawn(self.enemyJetSpawns[i])
+---
+-- @param #Mission07 self
+function Mission07:SpawnEnemyJets(count)
+
+  for i = 1, count do
+
+    self.enemyJets:SpawnAtAirbase(
+        self.moose.airbase:FindByName(self.moose.airbase.Caucasus.Mozdok),
+        self.moose.spawn.Takeoff.Cold)
+    
   end
-  return count
+
 end
 
 Mission07 = createClass(Mission, Mission07)
