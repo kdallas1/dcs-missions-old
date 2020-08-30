56c87fa3c1c90b3340dda31838adfca0206bdc0d
kdmp <nick@nbolton.com>
2020-08-30 12:28:13 +0100

    Add very basic unit tests for M3-4

--- Source/Horus/Test/Test.lua
+++ Source/Horus/Test/Test.lua
@@ -1,3 +1,7 @@
+dofile(baseDir .. "Horus/Test/TestMission01.lua")
+dofile(baseDir .. "Horus/Test/TestMission02.lua")
+dofile(baseDir .. "Horus/Test/TestMission03.lua")
+dofile(baseDir .. "Horus/Test/TestMission04.lua")
 dofile(baseDir .. "Horus/Test/TestMission05.lua")
 dofile(baseDir .. "Horus/Test/TestMission06.lua")
 dofile(baseDir .. "Horus/Test/TestMission07.lua")
@@ -7,6 +11,10 @@ dofile(baseDir .. "Horus/Test/TestMission09.lua")
 function Test_Horus()
   RunTests {
     "Horus",
+    Test_Mission01,
+    Test_Mission02,
+    Test_Mission03,
+    Test_Mission04,
     Test_Mission05,
     Test_Mission06,
     Test_Mission07,
